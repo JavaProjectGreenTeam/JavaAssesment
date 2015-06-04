e59fcc3a65e507381865f3b443dedf87bbc7836d
Nick <Nick@Nick-PC.Home>
2015-06-04 22:40:53 +1000

    Commit with change to FormMain

--- src/assesment/FormMain.java
+++ src/assesment/FormMain.java
@@ -26,6 +26,8 @@ public class FormMain extends javax.swing.JFrame {
     public FormMain() {
         initComponents();
         
+        //This is Nick's testing branch file
+        
         //this.setExtendedState(this.getExtendedState() | FormMain.MAXIMIZED_BOTH);
         
         comboDefaultArray.addAll(Arrays.asList("Select An Option..."));
